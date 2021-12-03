2a2c97f090c3fb924870318583292096decb1470
sbx_user1051 <sbx_user1051@169.254.46.201>
2021-12-03 18:12:04 +0000

    Fri Dec 03 18:12:04 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    
+    # Very first validate the input:
+        # - Validate that it is a string
+        # - Validate that it only contains appropriate letters (A,B,C,D)
+    
+    # First gather the number of each letter present in the string provided
+    # Then for special cases A and B, find the multiples (if any) this is of the special quantity, create a new category for this and store the value for the remainder
+    # For each letter (and new representation for offer values) multiply the value by the number of them in the products in the basket
     raise NotImplementedError()
+
