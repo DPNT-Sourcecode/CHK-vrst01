0f34c40c023c20ed7dded164b4863329e9a9eafe
sbx_user1051 <sbx_user1051@169.254.3.197>
2021-12-03 18:01:58 +0000

    Fri Dec 03 18:01:58 UTC 2021

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -4,9 +4,9 @@ import pytest
 
 class TestHLO():
     def test_hlo(self):
-        assert hello_solution.hello("clive") == "Hello, World!"
-    #     assert hello_solution.hello("clive") == "Hello clive"
-    # def test_non_string(self):
-    #     with pytest.raises(TypeError):
-    #         assert hello_solution.hello(1)
+        assert hello_solution.hello("clive") == "Hello, clive!"
+    def test_non_string(self):
+        with pytest.raises(TypeError):
+            assert hello_solution.hello(1)
+
 
