132d6d7ae8f2959d67869250c260719da0fb09cd
sbx_user1051 <sbx_user1051@169.254.3.197>
2021-12-03 17:44:25 +0000

    Fri Dec 03 17:44:25 UTC 2021

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,14 +1,24 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+class InputTypeError(Exception):
+    """Error to raise when one of the inputs is not of the correct type/"""
+    pass
+
+
+class InputRangeError(Exception):
+    """Error to raise when one of the inputs is not within the correct range."""
+    pass
+
+
 def compute(x, y):
     """Compute and return sum of integer inputs within range(0-100)"""
     if not (isinstance(x, int) and isinstance(y, int)):
-        raise TypeError("A value provided is not an integer")
+        raise InputTypeError("A value provided is not an integer")
     # TODO: The instructions for this task are ambiguous as they do not state which, if any, of the aspects
     # of the range are inclusive or exclusive; "between" is ambiguous, defaulting to python range functionality
     # which is inclusive of lower bound and exclusive of higher
     if x not in range(0,100):
-        raise ValueError("The input {} is not in range(0-100).".format(x))
+        raise InputRangeError("The input {} is not in range(0-100).".format(x))
     if y not in range(0,100):
-        raise ValueError("The input {} is not in range(0-100).".format(y))
+        raise InputRangeError("The input {} is not in range(0-100).".format(y))
     return x+y
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,10 @@
 from solutions.SUM import sum_solution
-
+import pytest
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+    def test_inclusive_exclusive_range_value(self):
+        assert sum_solution.compute(0, 99) == 99
+    def test_invalid_range_value(self):
+        with pytest.raises()
