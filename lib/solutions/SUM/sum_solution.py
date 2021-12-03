594c74d26e25c713c5e7defc4118e567c0631d0d
sbx_user1051 <sbx_user1051@169.254.3.197>
2021-12-03 17:41:25 +0000

    Fri Dec 03 17:41:25 UTC 2021

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,14 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    """Compute and return sum of integer inputs within range(0-100)"""
+    if not (isinstance(x, int) and isinstance(y, int)):
+        raise TypeError("A value provided is not an integer")
+    # TODO: The instructions for this task are ambiguous as they do not state which, if any, of the aspects
+    # of the range are inclusive or exclusive; "between" is ambiguous, defaulting to python range functionality
+    # which is inclusive of lower bound and exclusive of higher
+    if x not in range(0,100):
+        raise ValueError("The input {} is not in range(0-100).".format(x))
+    if y not in range(0,100):
+        raise ValueError("The input {} is not in range(0-100).".format(y))
+    return x+y
+
