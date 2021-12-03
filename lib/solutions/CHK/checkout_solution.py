00dc4d225b3b7d71187349d5d3a68043bc28d527
sbx_user1051 <sbx_user1051@169.254.46.201>
2021-12-03 18:18:04 +0000

    Fri Dec 03 18:18:04 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,18 +9,27 @@ def checkout(skus):
         # - Validate that it only contains appropriate letters (A,B,C,D)
         
     if not isinstance(skus, str):
-        raise ValueError("SKUS is not of type string, strings are expected type.")
+        return -1
     
     # Check length of string, then do count for each candidate letter, if sum total is different then an incorrect character is present
     total_count_of_skus = 0
+    count_of_skus = {}
     length_of_input_string = len(skus)
     
     for sku in ("ABCD"):
-        
+        sku_count = skus.count(sku)
+        total_count_of_skus += sku_count
+        count_of_skus[sku] = sku_count
+    if total_count_of_skus != length_of_input_string:
+        return -1
+    
+    # Calculate the offers for A and B
+    count_of_skus["A"].
     
     # First gather the number of each letter present in the string provided
     # Then for special cases A and B, find the multiples (if any) this is of the special quantity, create a new category for this and store the value for the remainder
     # For each letter (and new representation for offer values) multiply the value by the number of them in the products in the basket
     raise NotImplementedError()
+
 
 
